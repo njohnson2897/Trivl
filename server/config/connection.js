5f0f8796ca5fb22245586c7d67b33d544ac1a68e
Nate Johnson <njohnson2897@gmail.com>
2024-12-05 19:58:46 -0600

    altered connection.js to be suitable for deployment

--- server/config/connection.js
+++ server/config/connection.js
@@ -8,8 +8,14 @@ const sequelize = new Sequelize(
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-        host: 'localhost',
-        dialect: 'postgres'
+        host: process.env.DB_HOST,
+        dialect: 'postgres',
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        }
     }
 );
 
