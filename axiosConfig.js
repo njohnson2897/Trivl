c3494e9ff115f533b19321a6e7fc4ebf7ae712a4
Nate Johnson <njohnson2897@gmail.com>
2024-12-10 16:12:09 -0600

    adds optionality for axios base url

--- axiosConfig.js
+++ axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000',  // Change this if your server runs on a different port or domain
+  baseURL: 'http://localhost:5000' || 'https://trivl.onrender.com/',  // Change this if your server runs on a different port or domain
 });
 
 export default instance;
