ddb0cd3c3e6b3a65c62ef10c7f87b58589421016
Nate Johnson <njohnson2897@gmail.com>
2026-07-21 15:03:23 -0500

    updates API base url to railway backend in accordance with move away from render

--- axiosConfig.js
+++ axiosConfig.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
   baseURL:
     process.env.NODE_ENV === "production"
-      ? "https://trivl.onrender.com/"
+      ? "https://trivl.up.railway.app"
       : "http://localhost:5000", // Use the correct base URL based on the environment
 });
 
